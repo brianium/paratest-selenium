aebc98c9e699d2b7b6de480d7a87779a5b73dce4
Brian Scaturro <scaturrob@gmail.com>
2012-10-26 11:35:47 -0400

    add single chrome test

--- test/TodoTest.php
+++ test/TodoTest.php
@@ -1,15 +1,19 @@
-<?php
+<?php 
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Todos.php';
 
 class TodoTest extends PHPUnit_Extensions_Selenium2TestCase
 {
-    private $todos;
+    protected $todos;
+
+    public static $browsers = array(
+        array(
+            'browserName' => 'chrome'
+        )
+    );
 
     public function setUp()
     {
-        PHPUnit_Extensions_Selenium2TestCase::shareSession(true);
-        $this->setBrowser('firefox');
         $this->setBrowserUrl('http://backbonejs.org/examples/todos/');
         $this->todos = new Todos($this->prepareSession());
     }
@@ -90,4 +94,50 @@ class TodoTest extends PHPUnit_Extensions_Selenium2TestCase
         $this->assertEquals(0, sizeof($this->todos->getItems()));
     }
 
+    public function testDestroyOneOfTwoUpdatesCountText()
+    {
+        $this->todos->addTodos(array('one', 'two'));
+        $this->todos->getItemDestroy(0, array_shift($this->todos->getItems()))->click();
+        $this->assertEquals('1 item left', $this->todos->getTodoCount()->text());
+    }
+
+    public function testDestroyLastItemHidesMainAndFooter()
+    {
+        $this->todos->addTodo("make sure main and footer are hidden");
+        $this->todos->getItemDestroy(0, array_shift($this->todos->getItems()))->click();
+        $this->assertRegExp('/display: none;[\s]*/', $this->todos->getMain()->attribute('style'));
+        $this->assertRegExp('/display: none;[\s]*/', $this->todos->getFooter()->attribute('style'));
+    }
+
+    public function testOneTodoCheckedShowsCorrectClearButtonText()
+    {
+        $this->todos->addTodos(array('one', 'two'));
+        $this->todos->getItemCheckbox(array_shift($this->todos->getItems()))->click();
+        $this->assertEquals('Clear 1 completed item', $this->todos->getClearButton()->text());
+    }
+
+    public function testTwoTodosCheckedShowsCorrectClearButtonText()
+    {
+        $this->todos->addTodos(array('one', 'two'));
+        $this->todos->getToggleAll()->click();
+        $this->assertEquals('Clear 2 completed items', $this->todos->getClearButton()->text());
+    }
+
+    public function testClearButtonHidesMainAndFooter()
+    {
+        $this->todos->addTodos(array('one', 'two'));
+        $this->todos->getToggleAll()->click();
+        $this->todos->getClearButton()->click();
+        $this->assertRegExp('/display: none;[\s]*/', $this->todos->getMain()->attribute('style'));
+        $this->assertRegExp('/display: none;[\s]*/', $this->todos->getFooter()->attribute('style'));
+    }
+
+    public function testEditTodo()
+    {
+        $this->todos->addTodo("make sure todo is editable");
+        $todo = array_shift($this->todos->getItems());
+        $this->todos->switchToEdit(0, $todo);
+        $this->keys("appears its fine\n");
+        $this->assertEquals('appears its fine', $todo->text());
+    }
 }
--- test/Todos.php
+++ test/Todos.php
@@ -35,11 +35,25 @@ class Todos
                     ->value('li'));
     }
 
+    public function getItemView($item)
+    {
+        return $item->element($this->session
+                    ->using('css selector')
+                    ->value('.view'));
+    }
+
     public function getItemCheckbox($item)
     {
         return $item->element($this->session
                     ->using('css selector')
                     ->value('.view > .toggle'));
+    }
+
+    public function getItemEditField($item)
+    {
+        return $item->element($this->session
+                    ->using('css selector')
+                    ->value('.edit'));
     }
 
     public function getItemDestroy($index, $item)
@@ -58,10 +72,40 @@ class Todos
                     ->value('.view > .destroy'));
     }
 
+    public function switchToEdit($index, $item)
+    {
+        //double click not implemented
+        $script  = "var list = document.getElementById('todo-list');";
+        $script .= "var item = list.getElementsByTagName('li')[$index];";
+        $script .= "item.className = 'editing'";
+        $this->session->execute(array(
+            'script' => $script,
+            'args' => array()
+        ));
+        $edit = $this->getItemEditField($item);
+        $edit->clear();
+        $edit->click();
+    }
+
     public function getTodoCount()
     {
         return $this->session->byId("todoapp")
                     ->element($this->session->using('css selector')
                     ->value("footer > .todo-count"));
+    }
+
+    public function getMain()
+    {
+        return $this->session->byId("main");
+    }
+
+    public function getFooter()
+    {
+        return $this->session->byCssSelector("footer");
+    }
+
+    public function getClearButton()
+    {
+        return $this->session->byId("clear-completed");
     }
 }
